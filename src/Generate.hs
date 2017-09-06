eface5d3b9b697e537c443c234fc2185eb85534a
Sönke Hahn <soenkehahn@gmail.com>
2017-09-06 01:48:49 -0400

    make sound more organ-like

--- src/Generate.hs
+++ src/Generate.hs
@@ -1,7 +1,15 @@
+{-# OPTIONS_GHC -Wall -Werror #-}
+
 import Loopnaut.Utils
 
 main :: IO ()
 main = generateLoop "loop.wav" loop
 
 loop :: Generator
-loop phase = sin (phase * 180) * 0.1
+loop phase = mix (map (\freq -> sin (phase * freq) * 0.2) [200, 300, 400, 500])
+
+mix :: [Double] -> Double
+mix list =
+  case list of
+    [] -> 0
+    _ -> sum list / fromIntegral (length list)
