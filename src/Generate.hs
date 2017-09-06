5e7e191d1455f88242da2b2c171bec1d822c5a80
Sönke Hahn <soenkehahn@gmail.com>
2017-09-06 01:26:04 -0400

    introduce loopTime

--- src/Generate.hs
+++ src/Generate.hs
@@ -1,7 +1,7 @@
-
+import Data.Ratio
 import Data.Vector.Storable as V
 import Prelude hiding (writeFile)
-import Sound.File.Sndfile (getFileInfo, writeFile, samplerate)
+import Sound.File.Sndfile (getFileInfo, samplerate, writeFile)
 import Sound.File.Sndfile.Buffer.Vector as V
 import System.Random
 
@@ -14,29 +14,30 @@ main = do
 overwrite :: FilePath -> IO ()
 overwrite file = do
   info <- getFileInfo file
-  _ <- writeFile info file =<< mkBuffer (samplerate info)
+  _ <- writeFile info file =<< mkBuffer (fromIntegral (samplerate info))
   return ()
 
-mkBuffer :: Int -> IO (V.Buffer Double)
+mkBuffer :: Integer -> IO (V.Buffer Double)
 mkBuffer samplerate = return $ toBuffer $ vector samplerate
 
 vector samplerate = (unfoldr (gen samplerate) initial)
 
-data State = State {
-  sampleCount :: Int,
-  phase :: Double
-}
+data State = State
+  { loopTime :: Rational
+  , phase :: Double
+  }
 
 initial :: State
 initial = State 0 0
 
-gen :: Int -> State -> Maybe (Double, State)
-gen samplerate (State sampleCount phase) =
-  if sampleCount >= samplerate * 5
+gen :: Integer -> State -> Maybe (Double, State)
+gen samplerate (State loopTime phase) =
+  if loopTime >= 1
     then Nothing
     else Just $
-      let newPhase = foldRadians (phase + tau * 1 / fromIntegral samplerate)
-      in (sin (phase * 180) * 0.1, State (sampleCount + 1) newPhase)
+         let newPhase = foldRadians (phase + tau * 1 / fromIntegral samplerate)
+         in ( sin (phase * 180) * 0.1
+            , State (loopTime + 1 % (5 * samplerate)) newPhase)
 
 foldRadians :: Double -> Double
 foldRadians x =
