22a79d41c5d023998e0232ef70ec726284ee841b
Sönke Hahn <soenkehahn@gmail.com>
2017-11-07 22:46:39 -0500

    remove unused includes

--- src/loop_buffer.c
+++ src/loop_buffer.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <unistd.h>
 #include <jack/jack.h>
 
 jack_port_t *output_port1, *output_port2;
