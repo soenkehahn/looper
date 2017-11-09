861232da6e5903f180191aef50535a8b40ad79b7
soenkehahn <SoenkeHahn@gmail.com>
2017-11-09 11:11:08 -0500

    isolate scopes of c test cases

--- test/loop_buffer_spec.c
+++ test/loop_buffer_spec.c
@@ -17,86 +17,98 @@ void assertEqual(float a, float b) {
   }
 }
 
-void test_create_empty_loopnaut() {
-  test("create_empty_loopnaut returns a silent sample");
-  struct loopnaut* loopnaut = create_empty_loopnaut();
-  assertEqual(get_next_sample(loopnaut), 0);
-  free(loopnaut);
-
-  test("create_empty_loopnaut returns silent samples on subsequent calls");
-  loopnaut = create_empty_loopnaut();
-  get_next_sample(loopnaut);
-  assertEqual(get_next_sample(loopnaut), 0);
-}
-
-void test_set_buffer() {
-  test("set_buffer allows to set a buffer to a constant");
-  struct loopnaut* loopnaut = create_empty_loopnaut();
-  float array[1] = {42.0};
-  set_buffer(loopnaut, array, 1);
-  get_next_sample(loopnaut);
-  assertEqual(get_next_sample(loopnaut), 42);
-  free(loopnaut);
-
-  test("set_buffer allows to set a buffer to a sequence");
-  loopnaut = create_empty_loopnaut();
-  float sequence[3] = {1, 2, 3};
-  set_buffer(loopnaut, sequence, 3);
-  get_next_sample(loopnaut);
-  assertEqual(get_next_sample(loopnaut), 1);
-  assertEqual(get_next_sample(loopnaut), 2);
-  assertEqual(get_next_sample(loopnaut), 3);
-  free(loopnaut);
-
-  test("set_buffer loops constants");
-  loopnaut = create_empty_loopnaut();
-  set_buffer(loopnaut, sequence, 3);
-  get_next_sample(loopnaut);
-  get_next_sample(loopnaut);
-  get_next_sample(loopnaut);
-  get_next_sample(loopnaut);
-  assertEqual(get_next_sample(loopnaut), 1);
-  assertEqual(get_next_sample(loopnaut), 2);
-  assertEqual(get_next_sample(loopnaut), 3);
-  free(loopnaut);
-
-  test("set_buffer loops sequences");
-  loopnaut = create_empty_loopnaut();
-  set_buffer(loopnaut, array, 1);
-  get_next_sample(loopnaut);
-  assertEqual(get_next_sample(loopnaut), 42);
-  free(loopnaut);
-
-  test("set_buffer waits for the current sequence to finish before switching");
-  loopnaut = create_empty_loopnaut();
-  float a[3] = {1, 2, 3};
-  set_buffer(loopnaut, a, 3);
-  get_next_sample(loopnaut);
-  assertEqual(get_next_sample(loopnaut), 1);
-  float b[3] = {4, 5, 6};
-  set_buffer(loopnaut, b, 3);
-  assertEqual(get_next_sample(loopnaut), 2);
-  assertEqual(get_next_sample(loopnaut), 3);
-  assertEqual(get_next_sample(loopnaut), 4);
-  assertEqual(get_next_sample(loopnaut), 5);
-  assertEqual(get_next_sample(loopnaut), 6);
-  free(loopnaut);
-
-  test("set_buffer handles two calls to set_buffer gracefully");
-  loopnaut = create_empty_loopnaut();
-  float x[3] = {1, 2, 3};
-  set_buffer(loopnaut, x, 3);
-  float y[3] = {4, 5, 6};
-  set_buffer(loopnaut, y, 3);
-  get_next_sample(loopnaut);
-  assertEqual(get_next_sample(loopnaut), 4);
-  assertEqual(get_next_sample(loopnaut), 5);
-  assertEqual(get_next_sample(loopnaut), 6);
-  free(loopnaut);
-}
-
 int main() {
-  test_create_empty_loopnaut();
-  test_set_buffer();
+  {
+    test("create_empty_loopnaut returns a silent sample");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    assertEqual(get_next_sample(loopnaut), 0);
+    free(loopnaut);
+  }
+
+  {
+    test("create_empty_loopnaut returns silent samples on subsequent calls");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    get_next_sample(loopnaut);
+    assertEqual(get_next_sample(loopnaut), 0);
+  }
+
+  {
+    test("set_buffer allows to set a buffer to a constant");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    float array[1] = {42.0};
+    set_buffer(loopnaut, array, 1);
+    get_next_sample(loopnaut);
+    assertEqual(get_next_sample(loopnaut), 42);
+    free(loopnaut);
+  }
+
+  {
+    test("set_buffer allows to set a buffer to a sequence");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    float sequence[3] = {1, 2, 3};
+    set_buffer(loopnaut, sequence, 3);
+    get_next_sample(loopnaut);
+    assertEqual(get_next_sample(loopnaut), 1);
+    assertEqual(get_next_sample(loopnaut), 2);
+    assertEqual(get_next_sample(loopnaut), 3);
+    free(loopnaut);
+  }
+
+  {
+    test("set_buffer loops constants");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    float sequence[3] = {1, 2, 3};
+    set_buffer(loopnaut, sequence, 3);
+    get_next_sample(loopnaut);
+    get_next_sample(loopnaut);
+    get_next_sample(loopnaut);
+    get_next_sample(loopnaut);
+    assertEqual(get_next_sample(loopnaut), 1);
+    assertEqual(get_next_sample(loopnaut), 2);
+    assertEqual(get_next_sample(loopnaut), 3);
+    free(loopnaut);
+  }
+
+  {
+    test("set_buffer loops sequences");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    float array[1] = {42.0};
+    set_buffer(loopnaut, array, 1);
+    get_next_sample(loopnaut);
+    assertEqual(get_next_sample(loopnaut), 42);
+    free(loopnaut);
+  }
+
+  {
+    test("set_buffer waits for the current sequence to finish before switching");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    float a[3] = {1, 2, 3};
+    set_buffer(loopnaut, a, 3);
+    get_next_sample(loopnaut);
+    assertEqual(get_next_sample(loopnaut), 1);
+    float b[3] = {4, 5, 6};
+    set_buffer(loopnaut, b, 3);
+    assertEqual(get_next_sample(loopnaut), 2);
+    assertEqual(get_next_sample(loopnaut), 3);
+    assertEqual(get_next_sample(loopnaut), 4);
+    assertEqual(get_next_sample(loopnaut), 5);
+    assertEqual(get_next_sample(loopnaut), 6);
+    free(loopnaut);
+  }
+
+  {
+    test("set_buffer handles two calls to set_buffer gracefully");
+    struct loopnaut* loopnaut = create_empty_loopnaut();
+    float x[3] = {1, 2, 3};
+    set_buffer(loopnaut, x, 3);
+    float y[3] = {4, 5, 6};
+    set_buffer(loopnaut, y, 3);
+    get_next_sample(loopnaut);
+    assertEqual(get_next_sample(loopnaut), 4);
+    assertEqual(get_next_sample(loopnaut), 5);
+    assertEqual(get_next_sample(loopnaut), 6);
+    free(loopnaut);
+  }
+
   return result;
 }
