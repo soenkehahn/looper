8a883e56170a645fca9951de7f9c22023106cc10
Sönke Hahn <soenkehahn@gmail.com>
2018-02-11 17:44:28 -0500

    formatting police

--- src/CBindings/FFI.hs
+++ src/CBindings/FFI.hs
@@ -1,16 +1,15 @@
-
 module CBindings.FFI where
 
 import CBindings
-import Foreign.Ptr
 import Foreign.C.Types
+import Foreign.Ptr
 
-foreign import ccall "create_loopnaut" c_create_loopnaut :: IO (Ptr CLoopnaut)
+foreign import ccall "create_loopnaut" c_create_loopnaut ::
+               IO (Ptr CLoopnaut)
 
-foreign import ccall "set_buffer" c_set_buffer :: Ptr CLoopnaut -> Ptr CFloat -> Int -> IO ()
+foreign import ccall "set_buffer" c_set_buffer ::
+               Ptr CLoopnaut -> Ptr CFloat -> Int -> IO ()
 
 cBindings :: CBindings
-cBindings = CBindings {
-  create_loopnaut = c_create_loopnaut,
-  set_buffer = c_set_buffer
-}
+cBindings =
+  CBindings {create_loopnaut = c_create_loopnaut, set_buffer = c_set_buffer}
