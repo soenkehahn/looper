428e883b524deb8049420e49b4285ca5b7277ffd
Sönke Hahn <soenkehahn@gmail.com>
2019-12-25 17:12:07 +0100

    switch to portaudio

--- src/loop_buffer.c
+++ src/loop_buffer.c
@@ -1,8 +1,6 @@
+#include <portaudio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <jack/jack.h>
-
-jack_port_t *output_port1, *output_port2;
 
 struct loopnaut {
   float* array;
@@ -46,83 +44,76 @@ float get_next_sample(struct loopnaut* loopnaut) {
   return result;
 }
 
-// jack stuff
-
-int process(jack_nframes_t nframes, void* arg) {
-  struct loopnaut* loopnaut = arg;
-
-  jack_default_audio_sample_t* out1, * out2;
-  out1 = (jack_default_audio_sample_t*) jack_port_get_buffer(output_port1, nframes);
-  out2 = (jack_default_audio_sample_t*) jack_port_get_buffer(output_port2, nframes);
-
-  for(int i = 0; i < nframes; i++) {
-    float output = get_next_sample(loopnaut);
-    out1[i] = output;
-    out2[i] = output;
-  }
-
-  return 0;
-}
-
-void jack_shutdown(void *arg) {
-  fprintf(stderr, "jack_shutdown");
-  exit(1);
-}
-
-jack_client_t* init_client() {
-  const char *client_name = "loopnaut";
-  const char *server_name = NULL;
-  jack_options_t options = JackNullOption;
-  jack_status_t status;
-
-  jack_client_t *client;
-  client = jack_client_open(client_name, options, &status, server_name);
-  if (client == NULL) {
-    fprintf(stderr, "jack_client_open() failed, status = 0x%2.0x\n", status);
-    if (status & JackServerFailed) {
-      fprintf(stderr, "Unable to connect to JACK server\n");
-    }
-    exit(1);
-  }
-
-  if (status & JackServerStarted) {
-    fprintf(stderr, "JACK server started\n");
-  }
-
-  if (status & JackNameNotUnique) {
-    client_name = jack_get_client_name(client);
-    fprintf(stderr, "unique name `%s' assigned\n", client_name);
-  }
-
-  jack_on_shutdown(client, jack_shutdown, 0);
-
-  return client;
-}
+void start_portaudio(struct loopnaut* loopnaut);
 
 struct loopnaut* create_loopnaut() {
   struct loopnaut* loopnaut = create_empty_loopnaut();
-
-  jack_client_t* client = init_client();
-
-  jack_set_process_callback(client, process, loopnaut);
-
-  output_port1 = jack_port_register(client, "output1",
-    JACK_DEFAULT_AUDIO_TYPE,
-    JackPortIsOutput, 0);
-
-  output_port2 = jack_port_register(client, "output2",
-    JACK_DEFAULT_AUDIO_TYPE,
-    JackPortIsOutput, 0);
-
-  if ((output_port1 == NULL) || (output_port2 == NULL)) {
-    fprintf(stderr, "no more JACK ports available\n");
-    exit(1);
-  }
-
-  if (jack_activate(client)) {
-    fprintf(stderr, "cannot activate client\n");
-    exit(1);
-  }
-
+  start_portaudio(loopnaut);
   return loopnaut;
+}
+
+// portaudio stuff
+
+static int paCallback(
+  const void* inputBuffer,
+  void* outputBuffer,
+  unsigned long framesPerBuffer,
+  const PaStreamCallbackTimeInfo* timeInfo,
+  PaStreamCallbackFlags statusFlags,
+  void* userdata
+) {
+  struct loopnaut* loopnaut = (struct loopnaut*) userdata;
+  float* out = (float*) outputBuffer;
+
+  for(unsigned long i = 0; i < framesPerBuffer; i++) {
+    float sample = get_next_sample(loopnaut);
+    out[0] = sample; // left
+    out[1] = sample; // right
+    out += 2;
+  }
+
+  return paContinue;
+}
+
+void start_portaudio(struct loopnaut* loopnaut) {
+  PaStreamParameters outputParameters;
+  PaStream* stream;
+  PaError err;
+
+  err = Pa_Initialize();
+  if(err != paNoError) goto error;
+
+  outputParameters.device = Pa_GetDefaultOutputDevice();
+  if (outputParameters.device == paNoDevice) {
+    fprintf(stderr,"Error: No default output device.\n");
+    goto error;
+  }
+  outputParameters.channelCount = 2;
+  outputParameters.sampleFormat = paFloat32;
+  outputParameters.suggestedLatency =
+    Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
+
+  err = Pa_OpenStream(
+    &stream,
+    NULL,
+    &outputParameters,
+    44100,
+    1024,
+    paClipOff,
+    paCallback,
+    loopnaut
+  );
+  if(err != paNoError) goto error;
+
+  err = Pa_StartStream(stream);
+  if(err != paNoError) goto error;
+
+  return;
+
+error:
+  Pa_Terminate();
+  fprintf(stderr, "An error occured while using the portaudio stream\n");
+  fprintf(stderr, "Error number: %d\n", err);
+  fprintf(stderr, "Error message: %s\n", Pa_GetErrorText(err));
 }
--- test/LoopBufferSpec.hs
+++ test/LoopBufferSpec.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module LoopBufferSpec where
 
 import Control.Exception
-import System.Process
+import Development.Shake
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "loop_buffer" $ do
     it "c test-suite" $ do
+      Stdout (words -> libsFlags) <- cmd "pkg-config --libs portaudio-2.0"
       bracket
-        (callCommand "gcc test/loop_buffer_spec.c -ljack -o test/a.out")
-        (\() -> callCommand "rm test/a.out")
-        (\() -> callCommand "test/a.out")
+        (unit $ cmd "gcc test/loop_buffer_spec.c" libsFlags "-o test/a.out")
+        (\ () -> unit $ cmd "rm test/a.out")
+        (\ () -> unit $ cmd "test/a.out")
